075313acab9bfe3d30acc27e770413a8a0b0dc85
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-29 04:38:13 +0545

    initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="mainNav">Hello world</div>
+      <div className="mainContent">
+        <h2>Your Favourites</h2>
+        <div className="cardsWrap">
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80"
+                alt="Pic 1"
+              />
+            </div>
+            <div className="cardContent">
+              <h4>Liked Songs</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
