6ec46a6e0e23b4398efe471cb3c4faf17dc53939
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-29 05:37:45 +0545

    mainpage designed

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -22,6 +22,24 @@ const Main = () => {
             </div>
           </div>
         </div>
+
+        <h3>Focus</h3>
+        <div className="cardsWrap">
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80"
+                alt="Pic 1"
+              />
+            </div>
+            <div className="cardContent">
+              <h4>Liked Songs</h4>
+              <span className="playIcon">
+                <i class="fas fa-play" />
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
