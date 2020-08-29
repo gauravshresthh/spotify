e73f1a77a513468df4427f0871f702a6b7d19de2
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-29 06:15:37 +0545

    markups completed on main page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,8 +5,8 @@ const Main = () => {
     <div className="main">
       <div className="mainNav">Hello world</div>
       <div className="mainContent">
-        <h2>Your Favourites</h2>
         <div className="cardsWrap">
+          <h1>Your Favourites</h1>
           <div className="card">
             <div className="cardImage">
               <img
@@ -15,7 +15,7 @@ const Main = () => {
               />
             </div>
             <div className="cardContent">
-              <h4>Liked Songs</h4>
+              <h3>Liked Songs</h3>
               <span className="playIcon">
                 <i class="fas fa-play" />
               </span>
@@ -23,20 +23,40 @@ const Main = () => {
           </div>
         </div>
 
-        <h3>Focus</h3>
         <div className="cardsWrap">
-          <div className="card">
-            <div className="cardImage">
-              <img
-                src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80"
-                alt="Pic 1"
-              />
+          <h2>Focus</h2>
+          <p className="subTitle">Music to help you concentrate.</p>
+          <span>See all</span>
+          <div className="cardsWrapInner">
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Music to help you concentrate</span>
+                <span className="playIcon">
+                  <i class="fas fa-play" />
+                </span>
+              </div>
             </div>
-            <div className="cardContent">
-              <h4>Liked Songs</h4>
-              <span className="playIcon">
-                <i class="fas fa-play" />
-              </span>
+            <div className="card">
+              <div className="cardImage">
+                <img
+                  src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80"
+                  alt="Pic 1"
+                />
+              </div>
+              <div className="cardContent">
+                <h3>Music for concentration</h3>
+                <span>Music to help you concentrate</span>
+                <span className="playIcon">
+                  <i class="fas fa-play" />
+                </span>
+              </div>
             </div>
           </div>
         </div>
